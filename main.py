57882c8deced088f4ffb4fbd6beea9e499ab754e
jomo <github@jomo.tv>
2014-05-17 00:14:40 +0200

    rename mysql-connector

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ __plugin_mainclass__ = "foobar"
 
 import sys
 
+print("Loading RedstonerUtils...")
+
 # damn pythonloader changed this...
 sys.path += ['', '/usr/lib/python2.7', '/usr/lib/python2.7/plat-linux2', '/usr/lib/python2.7/lib-tk', '/usr/lib/python2.7/lib-old', '/usr/lib/python2.7/lib-dynload', '/usr/local/lib/python2.7/dist-packages', '/usr/lib/python2.7/dist-packages', '/usr/lib/pymodules/python2.7', '/usr/lib/pyshared/python2.7']
 
