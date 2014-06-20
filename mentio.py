72219d9e27940cf012e9b6cd9296d9eed32a843d
Louis Vogt <l.uisv.gt@icloud.com>
2014-06-20 22:16:17 +0200

    Fixed indention. Finally.

--- mentio.py
+++ mentio.py
@@ -6,13 +6,13 @@ mio_permission = "utils.mio"
 
 @hook.event("player.PlayerChatEvent", "normal")
 def onChat(event):
-	symbol = u"\u272a"
-	sender = event.getPlayer()
-	messages = event.getMessage()
-	messagesList = messages.split(" ")
-	for message in messagesList:
-		for recipient in server.getOnlinePlayers().tolist():
-			if message[:3].lower() in recipient.getName().lower() and len(message) > 2:
-				msg(recipient, "&6" + symbol + " &f%s &6mentioned you" % sender.getDisplayName())
-				# Couldn't figure out how to do this
-				# recipient.playSound(recipient.getLocation(), Sound.CHICKEN_EGG_POP, 1, 1)
+  symbol = u"\u272a"
+  sender = event.getPlayer()
+  messages = event.getMessage()
+  messagesList = messages.split(" ")
+  for message in messagesList:
+    for recipient in server.getOnlinePlayers().tolist():
+      if message[:3].lower() in recipient.getName().lower() and len(message) > 2:
+        msg(recipient, "&6" + symbol + " &f%s &6mentioned you" % sender.getDisplayName())
+        # Couldn't figure out how to do this
+        # recipient.playSound(recipient.getLocation(), Sound.CHICKEN_EGG_POP, 1, 1)
