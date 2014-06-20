3095761138cfecf2a29474afe444c5d4e31a07a1
Louis Vogt <l.uisv.gt@icloud.com>
2014-06-20 21:56:30 +0200

    Added mention module

---
+++ mentio.py
@@ -0,0 +1,18 @@
+#pylint: disable=F0401
+from helpers import *
+
+
+mio_permission = "utils.mio"
+
+@hook.event("player.PlayerChatEvent", "normal")
+def onChat(event):
+	symbol = u"\u272a"
+	sender = event.getPlayer()
+	messages = event.getMessage()
+	messagesList = messages.split(" ")
+	for message in messagesList:
+		for recipient in server.getOnlinePlayers().tolist():
+			if message[:3].lower() in recipient.getName().lower() and len(message) > 2 and len(message) <= len(recipient.getName()):
+				msg(recipient, "&6" + symbol + " &f%s &6mentioned you" % sender.getDisplayName())
+				# Couldn't figure out how to done this
+				# recipient.playSound(recipient.getLocation(), Sound.CHICKEN_EGG_POP, 1, 1)
