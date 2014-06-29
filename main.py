ead78abd1cecfcf360bfcaca337b72f8c5ab5b20
jomo <github@jomo.tv>
2014-06-29 15:18:25 +0200

    move cycle plugin to new module; add features

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def onDisable():
 log("Loading RedstonerUtils...")
 
 # Import all modules
-modules = ["misc", "adminchat", "lagchunks", "reports", "chatgroups", "webtoken", "saylol", "skullclick", "mentio"]
+modules = ["misc", "adminchat", "lagchunks", "reports", "chatgroups", "webtoken", "saylol", "skullclick", "mentio", "cycle"]
 mod = {}
 for module in modules:
   try:
