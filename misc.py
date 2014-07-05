52f5f92e733ac48d7f4c904df3701ac7bf648452
jomo <github@jomo.tv>
2014-07-05 21:12:27 +0200

    add 0.4s delay to shearing

--- misc.py
+++ misc.py
@@ -1,5 +1,6 @@
 #pylint: disable=F0401
 from helpers import *
+from time import time as now
 import org.bukkit.inventory.ItemStack as ItemStack
 
 #
@@ -74,16 +75,22 @@ def onSudoCommand(sender, args):
 # Clicking redstone_sheep with shears will drop redstone + wool and makes a moo sound
 #
 
+last_shear = 0.0
+
 @hook.event("player.PlayerInteractEntityEvent")
 def onPlayerInteractEntity(event):
+  global last_shear
   if not event.isCancelled():
-    sender = event.getPlayer()
-    entity = event.getRightClicked()
-    if isPlayer(entity) and str(entity.getUniqueId()) == "ae795aa8-6327-408e-92ab-25c8a59f3ba1" and str(sender.getItemInHand().getType()) == "SHEARS" and str(sender.getGameMode()) == "CREATIVE":
-      for i in range(5):
-        entity.getWorld().dropItemNaturally(entity.getLocation(), ItemStack(bukkit.Material.getMaterial("REDSTONE")))
-        entity.getWorld().dropItemNaturally(entity.getLocation(), ItemStack(bukkit.Material.getMaterial("WOOL")))
-      sender.playSound(entity.getLocation(), "mob.cow.say", 1, 1)
+    shear_time = now()
+    if last_shear + 0.4 < shear_time:
+      last_shear = shear_time
+      sender = event.getPlayer()
+      entity = event.getRightClicked()
+      if isPlayer(entity) and str(entity.getUniqueId()) == "ae795aa8-6327-408e-92ab-25c8a59f3ba1" and str(sender.getItemInHand().getType()) == "SHEARS" and str(sender.getGameMode()) == "CREATIVE":
+        for i in range(5):
+          entity.getWorld().dropItemNaturally(entity.getLocation(), ItemStack(bukkit.Material.getMaterial("REDSTONE")))
+          entity.getWorld().dropItemNaturally(entity.getLocation(), ItemStack(bukkit.Material.getMaterial("WOOL")))
+        sender.playSound(entity.getLocation(), "mob.cow.say", 1, 1)
 
 
 #
