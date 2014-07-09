5846d0f7653dd181de5597392d173c72d03148cf
jomo <github@jomo.tv>
2014-07-10 00:52:32 +0200

    more questions

--- abot.py
+++ abot.py
@@ -1,16 +1,20 @@
 from helpers import *
 from re import compile as reg_compile
 
-rank_regex = "visitor|member|builder|trusted|helper|mod|admin"
+rank_regex = "visitor|member|builder|trusted|helper|mod|admin|owner|rank"
 
 faq_regex = [
-  # Asking for ranks or WE
-  "how.+ (get|be(come)?|).+ (%s|WorldEdit|WE|W.E.)" % rank_regex,
-  # Asking why p clear won't work
-  "why.+ can.+( /?p clear| clear.+plot)"
+  # ranks
+  "(how.+ (get|be(come)?|)|who is).+ (%s)" % rank_regex,
+  # WE
+  "((can|how|why).+ (have|haz|use|doesn|can'?t)).+ WorldEdit|WE|W.E.)"
+  # clearing plot
+  "(why|how).+ can.+( /?p clear| clear.+plot)",
+  # add someone to a plot
+  "how.+ add.+ plot"
 ]
 
-faq_regex = [reg_compile(reg) for reg in faq_regex]
+faq_regex = [reg_compile(reg.lower()) for reg in faq_regex]
 
 @hook.event("player.AsyncPlayerChatEvent", "low")
 def onChat(event):
@@ -22,4 +26,5 @@ def onChat(event):
       msg(sender, "&aIt looks like you aksed a question that is likely answered in our FAQ.")
       msg(sender, "&aPlease take a look at the &4&l/faq&a command and read through the pages.")
       event.setCancelled(True)
+      log("(Answerbot) hiding message from %s: '%s'" % (sender.getName(), message))
       break
