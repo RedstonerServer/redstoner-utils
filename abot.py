b9e73b58d736cf9e29b6ea62d7778b4ed0138c88
jomo <github@jomo.tv>
2014-07-10 23:08:23 +0200

    skip abot for players with 'utils.ignore_abot' permission

--- abot.py
+++ abot.py
@@ -19,12 +19,13 @@ faq_regex = [reg_compile(reg.lower()) for reg in faq_regex]
 @hook.event("player.AsyncPlayerChatEvent", "low")
 def onChat(event):
   sender  = event.getPlayer()
-  message = event.getMessage().lower()
-  for regex in faq_regex:
-    if regex.search(message):
-      plugHeader(sender, "AnswerBot")
-      msg(sender, "&aLooks like you're asking something that's likely in our FAQ.")
-      msg(sender, "&aTake a look at &4&l/faq&a and read through the pages.\n ") # trailing space required
-      event.setCancelled(True)
-      log("(hidden) %s: '%s'" % (sender.getName(), message))
-      break
+  if not sender.hasPermission("utils.ignore_abot"):
+    message = event.getMessage().lower()
+    for regex in faq_regex:
+      if regex.search(message):
+        plugHeader(sender, "AnswerBot")
+        msg(sender, "&aLooks like you're asking something that's likely in our FAQ.")
+        msg(sender, "&aTake a look at &4&l/faq&a and read through the pages.\n ") # trailing space required
+        event.setCancelled(True)
+        log("(hidden) %s: '%s'" % (sender.getName(), message))
+        break
