a38eb337606b25bb31f6ec3f4b403361d9470cb2
Dico <dico.karssiens@gmail.com>
2014-07-18 02:27:24 +0200

    Happy with this version/build. @Sheep: Don't fix my 'dicode'!

--- forcefield.py
+++ forcefield.py
@@ -5,12 +5,12 @@ from org.bukkit.util import Vector
 from math import sin
 
 ff_perm        = "utils.forcefield"
-pass_perm      = "utils.forcefield.pass"
-ff_prefix      = "&8[&aFF&8]"
+pass_perm      = "utils.forcefield.ignore"
+ff_prefix      = "&8[&bFF&8] "
 ff_users       = []
 whitelists     = {}        # {ff_owner_id: [white, listed, ids]}
 fd             = 6         # forcefield distance
-Xv             = 3.05 / fd # used in set_velocity_away(), this is more efficient.
+Xv             = 2.95 / fd # used in move_away(), this is more efficient.
 
 # /ff admin  is a future option I might implement
 
@@ -21,37 +21,34 @@ def on_forcefield_command(sender, args):
     noperm(sender)
     return True
 
-  cmd = args[0].lower()
+  if not args or args[0].upper() in ["ON", "OFF"]: # Toggle
+    forcefield_toggle(sender, args[:1])
+    return True
 
-  if not args or cmd == "toggle": # Toggle
-    forcefield_toggle(sender)
+  args[0] = args[0].upper() # If it gets to this point, there are argument(s).
+  if args[0] in ["WHITELIST", "WL", "WLIST"]: # Whitelist commands
+    if not args[1:] or args[1].upper() == "LIST":
+      whitelist_list(sender)
+      return True
 
-  elif cmd in ["whitelist", "wl", "wlist"]: # Whitelist commands
-
-    if len(args) >= 2:
-      arg = args[1].lower()
-
-      if arg == "list":
-        whitelist_list(sender)
-      elif arg == "clear":
-        whitelist_clear(sender)
-      elif arg in ["add", "+"]:
-        change_whitelist(sender, True, args[2:])
-      elif arg in ["remove", "delete", "rem", "del", "-"]:
-        change_whitelist(sender, False, args[2:])
-      else:
-        invalid_syntax(sender)
+    args[1] = args[1].upper() # If it gets too this point, there is a second argument.
+    if args[1] == "CLEAR":
+      whitelist_clear(sender)
+    elif args[1] in ["ADD", "+"]:
+      change_whitelist(sender, True, args[2:])
+    elif args[1] in ["REMOVE", "DELETE", "REM", "DEL", "-"]:
+      change_whitelist(sender, False, args[2:])
     else:
-      invalid_syntax(sender)
+      forcefield_header(sender, "&cInvalid syntax. Use &e/ff ? &cfor info.")
 
-  elif cmd in ["help", "?"]: # /forcefield help
+  elif args[0] in ["HELP", "?"]: # /forcefield help
     forcefield_help(sender)
   else:
-    invalid_syntax(sender)
+    forcefield_header(sender, "&cInvalid syntax. Use &e/ff ? &cfor info.")
   return True
 
 
-def change_whitelist(sender, add, names):
+def change_whitelist(sender, add, names): #Add names if add == True else Remove names.
   if names:
     sender_id = uid(sender)
     if sender_id not in whitelists:
@@ -59,43 +56,97 @@ def change_whitelist(sender, add, names):
 
     for name in names:
       player = server.getOfflinePlayer(name)
+<<<<<<< HEAD
+      if player.hasPlayedBefore():
+        player_id = str(player.getUniqueId())
+=======
       if player:
         player_id = uid(player)
+>>>>>>> 3c5b0d8114070baf198595a5fd3580308d5f7b4f
         pname     = player.getName()
-        sname     = sender.getDisplayName()
+        sname     = stripcolors(sender.getDisplayName())
 
         # add player to whitelist if not already added
         if add and player_id not in whitelists[sender_id]:
           if sender != player:
             whitelists[sender_id].append(player_id)
-            msg(sender, "%s &aAdded %s to your forcefield whitelist." % (ff_prefix, pname))
-            msg(player, "%s &a%s &aadded you to his forcefield whitelist." % (ff_prefix, sname))
+            forcefield_header(sender, "&bAdded &f%s &bto your forcefield whitelist." % pname)
+            forcefield_header(player, "&f%s &badded you to his forcefield whitelist." % sname)
           else:
-            msg(sender, "%s &cYou can't whitelist yourself." % ff_prefix)
+            forcefield_header(sender, "&cYou can't whitelist yourself.")
 
         # remove player from whitelist if whitelisted
         elif not add and player_id in whitelists[sender_id]:
           whitelists[sender_id].remove(player_id)
-          msg(sender, "%s &cRemoved %s from your forcefield whitelist." % (ff_prefix, pname))
-          msg(player, "%s &c%s &cremoved you from his forcefield whitelist." % (ff_prefix, sname))
+          forcefield_header(sender, "&cRemoved &f%s &cfrom your forcefield whitelist." % pname)
+          forcefield_header(player, "&f%s &cremoved you from his forcefield whitelist." % sname)
+
+        # player was already / not added to whitelist
+        else:
+          var = "already" if add == True else "not"
+          forcefield_header(sender, "&f%s &cwas %s in your forcefield whitelist!" % (pname, var))
 
       else:
-        msg(sender, "%s &cplayer %s &cwas not found." % (ff_prefix, name))
+        forcefield_header(sender, "&cplayer &f%s &cwas not found." % name)
   else:
-    msg(sender, "%s &cGive space-separated playernames." % ff_prefix)
+    forcefield_header(sender, "&cGive space-separated playernames.")
 
 
+<<<<<<< HEAD
+def whitelist_list(player):
+  player_id = str(player.getUniqueId())
+=======
 def whitelist_list(sender):
   sender_id = uid(sender)
+>>>>>>> 3c5b0d8114070baf198595a5fd3580308d5f7b4f
   count     = 0
-  msg(sender, "%s &aForceField Whitelist:" % ff_prefix)
-  for player_id in whitelists.get(sender_id, []):
+  forcefield_header(player, "&bForcefield whitelist:")
+  for user_id in whitelists.get(player_id, []):
     count += 1
-    msg(sender, "&a%s. &f%s" % (count, server.getOfflinePlayer(id_to_player(player_id)).getName()))
+    pname = server.getOfflinePlayer(id_to_player(user_id)).getName()
+    msg(player, "&b %s. &f%s" % (count, pname))
   if count == 0:
-    msg(sender, "&cYour whitelist has no entries.")
+    msg(player, "&c Your whitelist has no entries.")
 
 
+<<<<<<< HEAD
+def whitelist_clear(player):
+  player_id = str(player.getUniqueId())
+  if whitelists.get(player_id):
+    whitelists.pop(player_id)
+    forcefield_header(player, "&bForcefield whitelist cleared.")
+  else:
+    forcefield_header(player, "&cYou had no players whitelisted.")
+
+
+def forcefield_help(player):
+  msg(player, " ")
+  forcefield_header(player, "&b&l/Forcefield help:         %s" % forcefield_check(player))
+  msg(player, "&b You can use the forcefield to keep players on distance.")
+  msg(player, "&b Commands:")
+  msg(player, "&b 1. &6/ff &ohelp &b:                         aliases: &6?")
+  msg(player, "&b 2. &6/ff &o(on off)")
+  msg(player, "&b 3. &6/ff &owhitelist (list) &b:              aliases: &6wlist, wl")
+  msg(player, "&b 4. &6/ff wl &oclear")
+  msg(player, "&b 5. &6/ff wl &oadd <players> &b:          aliases: &6+")
+  msg(player, "&b 6. &6/ff wl &oremove <players> &b:     aliases: &6delete, rem, del, - \n")
+
+
+def forcefield_check(player): # Returns a string to tell the player its forcefield status
+  return "&eYour forcefield is %s" % "&2ON" if str(player.getUniqueId()) in ff_users else "&cOFF"
+
+
+def forcefield_toggle(player, arg): # arg is a list with max 1 string
+  player_id = str(player.getUniqueId())
+  enabled   = player_id in ff_users
+  argoff    = arg[0].upper() == "OFF" if arg else False
+  if enabled and (not arg or argoff): # 3 possibilities for arg: [], ["OFF"], ["ON"]. This is the most efficient way. (Case insensitive)
+    ff_users.remove(player_id)
+    forcefield_header(player, "&bForcefield toggle: &cOFF")
+  elif not enabled and not argoff:
+    ff_users.append(player_id)
+    forcefield_header(player, "&bForcefield toggle: &2ON")
+=======
 def whitelist_clear(sender):
   sender_id = uid(sender)
   if whitelists.get(sender_id):
@@ -122,31 +173,13 @@ def forcefield_toggle(sender):
   if sender_id in ff_users:
     ff_users.remove(sender_id)
     msg(sender, "%s &aForceField toggle: &cOFF" % ff_prefix)
+>>>>>>> 3c5b0d8114070baf198595a5fd3580308d5f7b4f
   else:
-    ff_users.append(sender_id)
-    msg(sender, "%s &aForceField toggle: &2ON" % ff_prefix)
+    forcefield_header(player, "&cYour forcefield is already %s!" % arg[0].upper())
 
 
-def invalid_syntax(sender):
-  msg(sender, "%s &cInvalid syntax. Use &e/ff ? &cfor info." % ff_prefix)
-
-
-def move_away(player, entity):
-  """
-  Pushes entity away from player
-  """
-
-  player_loc = player.getLocation()
-  entity_loc = entity.getLocation()
-
-  dx = entity_loc.getX() - player_loc.getX()
-  vx = sin(Xv * dx)
-  dy = entity_loc.getY() - player_loc.getY()
-  vy = sin(Xv * dy)
-  dz = entity_loc.getZ() - player_loc.getZ()
-  vz = sin(Xv * dz)
-  entity.setVelocity(Vector(vx , vy, vz))
-  # We don't want to go above max_speed, and we dont want to divide by 0.
+def forcefield_header(player, message):
+  msg(player, "%s %s" % (ff_prefix, message))
 
 
 #--------------------------------------------------------------------------------------------------------#
@@ -158,15 +191,15 @@ def on_move(event):
   if is_creative(player):
     player_id = uid(player)
 
-    # moving player has forcefield, nerby player should be moved away
+    # moving player has forcefield, nearby player should be moved away
     if player_id in ff_users:
       for entity in player.getNearbyEntities(fd, fd, fd):
         whitelisted = (uid(entity) in whitelists.get(player_id, []))
         if is_player(entity) and not entity.hasPermission(pass_perm) and not whitelisted:
           move_away(player, entity)
 
-    # nerby player has forcefield, moving player should be moved away
-    if player.hasPermission(pass_perm):
+    # nearby player has forcefield, moving player should be moved away
+    if not player.hasPermission(pass_perm):
       for entity in player.getNearbyEntities(fd, fd, fd):
         entity_id   = uid(entity)
         ff_enabled  = (entity_id in ff_users)
@@ -175,12 +208,34 @@ def on_move(event):
           move_away(entity, player)
 
 
+def move_away(player, entity):
+  # Pushes entity away from player
+
+  player_loc = player.getLocation()
+  entity_loc = entity.getLocation()
+
+  dx = entity_loc.getX() - player_loc.getX()
+  vx = sin(Xv * dx)
+  dy = entity_loc.getY() - player_loc.getY()
+  vy = sin(Xv * dy)
+  dz = entity_loc.getZ() - player_loc.getZ()
+  vz = sin(Xv * dz)
+  entity.setVelocity(Vector(vx , vy, vz))
+
+
 #--------------------------------------------------------------------------------------------------------#
 
 
 @hook.event("player.PlayerQuitEvent")
 def on_quit(event):
+<<<<<<< HEAD
+  player    = event.getPlayer()
+  player_id = str(player.getUniqueId())
+  if player_id in ff_users:
+    ff_users.remove(player_id)
+=======
   player = event.getPlayer()
   uid    = uid(player)
   if uid in ff_users:
     ff_users.remove(uid)
+>>>>>>> 3c5b0d8114070baf198595a5fd3580308d5f7b4f
