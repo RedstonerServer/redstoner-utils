41d9f719f9927810526ae57f2a50807e83108737
Gap <l.uisv.gt@icloud.com>
2014-12-21 15:25:42 +0100

    Enables ACT for players who are set to offline

--- imout.py
+++ imout.py
@@ -1,5 +1,6 @@
 import thread
 from helpers import *
+from adminchat import *
 
 imout_toggle_list = []
 
@@ -7,13 +8,20 @@ imout_toggle_list = []
 def on_imout_command(sender, args):
   if sender.hasPermission("utils.imout"):
     name = sender.getName()
-    symbol = "&a+"
+    symbol = "&a&l+"
     if name in imout_toggle_list:
       msg(sender, "&eWelcome back! You are no longer hidden")
-      imout_toggle_list.remove(name)
+      msg(sender, "&6We disabled /act for you!")
+      if name in imout_toggle_list:
+        imout_toggle_list.remove(name)
+      if name in ac_toggle_list:
+        ac_toggle_list.remove(name)
     else:
-      symbol = "&c-"
+      symbol = "&c&l-"
       msg(sender, "&eYou just left... Or didn't you?")
       imout_toggle_list.append(name)
+      if name not in ac_toggle_list:
+        msg(sender, "&6We enabled /act for you!")
+        ac_toggle_list.append(name)
 
-    broadcast(None, "&l%s &7%s" % (symbol, name))
+    broadcast(None, "%s &7%s" % (symbol, name))
