c1a69c6a62167ebf7eaf3b20d0804b5d0e12881f
PanFritz <redstonenoobpan@gmail.com>
2015-01-17 05:31:32 +0100

    Fixed, upping snowball damaga

--- snowbrawl.py
+++ snowbrawl.py
@@ -22,7 +22,7 @@ def onHit(event):
         print "Not in minigames"
         return
     print "all good."
-    event.getEntity().getWorld().createExplosion(event.getEntity().getLocation().getX(), event.getEntity().getLocation().getY(),event.getEntity().getLocation().getZ(),5F,False,True)
+    event.getEntity().getWorld().createExplosion(event.getEntity().getLocation().getX(), event.getEntity().getLocation().getY(),event.getEntity().getLocation().getZ(),float(5),False,True)
 
         
 
