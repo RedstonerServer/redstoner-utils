73594375248ee80b4a1e5e3006ef363ee7afd775
PanFritz <redstonenoobpan@gmail.com>
2015-04-16 12:40:30 +0200

    Fix missing var

--- adminnotes.py
+++ adminnotes.py
@@ -124,9 +124,9 @@ def adminnotes_command(sender, command, label, args):
 
 
 @hook.event("player.PlayerJoinEvent", "monitor")
-def on_an_join(event):
-    if not sender.hasPermission(an_permission):
-        noperm(sender)
+def on_an_join(event):	
+    if not event.getPlayer().hasPermission(an_permission):
+        noperm(event.getPlayer())
         return
     if len(notes) > 0:
         msg(event.getPlayer(), "&cThere are currently %s open notes!" % len(notes))
