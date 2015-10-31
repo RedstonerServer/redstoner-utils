3f677467fced35b2084c01fa1173af88dadc7263
NEMESIS13cz <seke94@seznam.cz>
2015-10-31 19:24:33 +0100

    Fixed conflicts with chatalias

--- mentio.py
+++ mentio.py
@@ -13,7 +13,7 @@ def saveMentions():
     save_json_file("mentio", mentions)
 
 
-@hook.event("player.AsyncPlayerChatEvent", "high")
+@hook.event("player.AsyncPlayerChatEvent", "normal")
 def onChat(event):
     if not event.isCancelled():
         sender     = event.getPlayer()
